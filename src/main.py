97b03101f059c2e0ac0f0b72bb357f3c8755f641
nurulariffah <nurulariffah@graduate.utm.my>
2026-06-01 11:26:24 +0000

    Fix main function indentation

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import pandas as pd
+
 def load_data():
-# Example DataFrame
-data = {'A': [1, 2, 3], 'B': [4, 5, 6]}
-return pd.DataFrame(data)
+    # Example DataFrame
+    data = {'A': [1, 2, 3], 'B': [4, 5, 6]}
+    return pd.DataFrame(data)
