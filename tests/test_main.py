3a60d9a97993ff93402cbc92aec8b3ec1e8e0d5c
nurulariffah <nurulariffah@graduate.utm.my>
2026-06-01 11:26:15 +0000

    Add analytical function and automated tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,11 +1,12 @@
 import sys
 import os
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+# Add src folder to Python path
+sys.path.insert(0,
+os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 
 from main import load_data
 
 def test_load_data():
-    df = load_data()
-    print(f"DataFrame shape: {df.shape}")
-    assert not df.empty, "DataFrame should not be empty"
+df = load_data() print(f"DataFrame shape: {df.shape}")
+assert not df.empty, "DataFrame should not be empty"
